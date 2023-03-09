4122b04a7372d0c5dc75bb231f154a5ac7af0a3e
angel.ignatov.ignatov <angel.ignatov.ignatov@gmail.com>
2023-03-09 09:58:44 +0000

    Countdown Timer

---
+++ script.js
@@ -0,0 +1,27 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const cBirthday = "28 Feb 2022";
+
+function countdown() {
+    const cBirthdayDate = new Date(cBirthday);
+    const currentDate = new Date();
+
+    const totalSeconds = new Date(cBirthdayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
